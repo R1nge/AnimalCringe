79c6127d4502341b5b7b79bb4a920cbc65117019
R1nge <r1ngegamedev@gmail.com>
2023-11-29 13:50:46 +0300

    Fixed kill popup

--- Assets/_Assets/Scripts/Misc/KillPopup.cs
+++ Assets/_Assets/Scripts/Misc/KillPopup.cs
@@ -1,23 +1,36 @@
-﻿using _Assets.Scripts.Services.Lobbies;
+﻿using System.Collections;
 using TMPro;
 using UnityEngine;
-using VContainer;
 
 namespace _Assets.Scripts.Misc
 {
     public class KillPopup : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private float hideDelay = 1f;
+        [SerializeField] private TextMeshProUGUI popupText;
+        private Coroutine _hideCoroutine;
+        private YieldInstruction _hide;
+
+        private void Awake() => _hide = new WaitForSeconds(hideDelay);
 
 
         public void Show(NetworkString text)
         {
-            _text.text = text;
+            if (_hideCoroutine != null)
+            {
+                StopCoroutine(_hideCoroutine);
+            }
+
+            popupText.text = text;
+            _hideCoroutine = StartCoroutine(Hide_C());
         }
 
-        public void Hide()
+        private IEnumerator Hide_C()
         {
-            _text.text = string.Empty;
+            yield return _hide;
+            Hide();
         }
+
+        private void Hide() => popupText.text = string.Empty;
     }
 }
